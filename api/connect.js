337af54b92b00e7d30d58f5d0f4592a800ebca54
Dev240204 <dev.ace21@sot.pdpu.ac.in>
2024-08-01 19:28:41 +0530

    Post route for inserting new user

--- api/connect.js
+++ api/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
     try{
+        console.log(process.env.MONGODB_URI)
         mongoose.connect(process.env.MONGODB_URI)
         .then(() => {
             console.log('Connected to MongoDB')

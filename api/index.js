88b6c3686fd15828696dc5ef487dadeca330292c
Dev240204 <dev.ace21@sot.pdpu.ac.in>
2024-08-01 18:33:45 +0530

    Mongo connection

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000
 
 app.get('/',(req,res) => {
+    console.log(process.env.MONGODB_URI)
     res.send('Hello World')
+    
 })
 
 app.listen(port,(req,res)=>{

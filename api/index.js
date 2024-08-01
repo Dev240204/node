4b750b1c794746379f8f994650760dd34ba25b34
Dev240204 <dev.ace21@sot.pdpu.ac.in>
2024-08-01 19:01:42 +0530

    Post route for inserting new user

--- api/index.js
+++ api/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const app = express();
-const port = 3000
+const bcrypt = require('bcrypt')
 const connect = require('./connect')
 const Users = require('../models/Users')
+const app = express();
+const port = 3000
 
 connect()
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 app.get('/',(req,res) => {
     res.send('Hello World')
+})
+
+app.post("/user", async (req,res) => {
+    try{
+        const user = req.body
+        const response = await Users.findOne({username: user.username})
+        if(response){
+            res.send('User already exists')
+        }
+        const hashpassword = await bcrypt.hash(user.password,10)
+        const newUser = new Users({
+            username: user.username,
+            password: hashpassword
+        })
+        newUser.save()
+        res.status(200).send('User created successfully')
+    }catch(err){
+        console.log("Error: ",err)
+    }
 })
 
 app.listen(port,(req,res)=>{
